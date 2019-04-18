7d5a62d3cbffe3a62249511cfe38998f0f1290f3
Esteban <goldmuros@yahoo.com>
2019-04-18 18:35:52 -0300

    Se agrego el logout de la app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <v-app>
+    <v-toolbar dark dense class="light-green darken-3">
+      <v-toolbar-title>{{title}}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items v-if="logueado">
+        <v-tooltip left>
+          <v-btn fab small dark class="darken-3" slot="activator" @click="logout">
+            <v-icon dark>power_settings_new</v-icon>
+          </v-btn>
+          <span>Salir del sistema</span>
+        </v-tooltip>
+      </v-toolbar-items>
+    </v-toolbar>
     <div>
       <router-view></router-view>
     </div>
@@ -8,6 +20,28 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      title: 'Meta App'
+    }
+  },
+  computed: {
+    logueado () {
+      let page = this.$store.getters.getActivePage
+
+      if (page !== null && page !== '')
+        return true
+
+      return false
+    }
+  },
+  methods: {
+    logout () {
+      this.$store.commit('logout')
+
+      this.$router.push('/')
+    }
+  }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -43,6 +43,11 @@ export default new Vuex.Store({
           state.piezas.push(pieza.data())
         })        
       })
+    },
+    logout (state) {
+      state.activePage = ''
+      state.user.rol = ''
+      state.user.name = ''
     }
   },
   actions: {
