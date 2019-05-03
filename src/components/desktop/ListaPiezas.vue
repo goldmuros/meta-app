f9d2a6bb453ce320f7055deec68e3d66174ddbb3
Esteban <goldmuros@yahoo.com>
2019-05-02 21:18:29 -0300

    Media querys y pantalla para distintos dispositivos

---
+++ src/components/desktop/ListaPiezas.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-data-table
+    v-model="selected"
+    :headers="headers"
+    :items="piezas"
+    select-all
+    item-key="operador"
+    class="elevation-1"
+    :pagination.sync="pagination"
+  >
+    <template v-slot:headers="props">
+      <tr>
+        <th>
+          <v-checkbox
+            :input-value="props.all"
+            :indeterminate="props.indeterminate"
+            primary
+            hide-details
+            @click.stop="toggleAll"
+          ></v-checkbox>
+        </th>
+        <th
+          v-for="header in props.headers"
+          :key="header.text"
+          :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
+          @click="changeSort(header.value)"
+        >
+          <v-icon small>arrow_upward</v-icon>
+          {{ header.text }}
+        </th>
+      </tr>
+    </template>
+    <template v-slot:items="props">
+      <tr :active="props.selected" @click="props.selected = !props.selected">
+        <td>
+          <v-checkbox
+            :input-value="props.selected"
+            primary
+            hide-details
+          ></v-checkbox>
+        </td>
+        <td>{{ props.item.name }}</td>
+        <td class="text-xs-right">{{ props.item.maquina }}</td>
+        <td class="text-xs-right">{{ props.item.dimension }}</td>
+        <td class="text-xs-right">{{ props.item.fallo }}</td>
+        <td class="text-xs-right">{{ props.item.observaciones }}</td>
+      </tr>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      pagination: {
+        sortBy: 'operador'
+      },
+      selected: [],
+      headers: [
+        { text: 'Operador', value: 'operador'
+        },
+        { text: 'Máquina', value: 'maquina' },
+        { text: 'Dimensión', value: 'dimension' },
+        { text: 'Fallo', value: 'fallo' },
+        { text: 'Observaciones', value: 'observaciones' }
+      ]
+    }
+  },
+  computed: {
+    piezas () {
+      return this.$store.getters.getListPiezas
+    }
+  },
+  methods: {
+    toggleAll () {
+      if (this.selected.length) this.selected = []
+      else this.selected = this.desserts.slice()
+    },
+    changeSort (column) {
+      if (this.pagination.sortBy === column) {
+        this.pagination.descending = !this.pagination.descending
+      } else {
+        this.pagination.sortBy = column
+        this.pagination.descending = false
+      }
+    }
+  },
+  created() {
+    this.$store.commit('obtenerPiezas')
+  }
+}
+</script>
--- src/views/Ingenieria.vue
+++ src/views/Ingenieria.vue
@@ -1,111 +1,36 @@
 <template>
   <v-layout row justify-center>
-    <v-data-table
-      v-model="selected"
-      :headers="headers"
-      :items="piezas"
-      select-all
-      item-key="operador"
-      class="elevation-1"
-      :pagination.sync="pagination"
-    >
-      <template v-slot:headers="props">
-        <tr>
-          <th>
-            <v-checkbox
-              :input-value="props.all"
-              :indeterminate="props.indeterminate"
-              primary
-              hide-details
-              @click.stop="toggleAll"
-            ></v-checkbox>
-          </th>
-          <th
-            v-for="header in props.headers"
-            :key="header.text"
-            :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
-            @click="changeSort(header.value)"
-          >
-            <v-icon small>arrow_upward</v-icon>
-            {{ header.text }}
-          </th>
-        </tr>
-      </template>
-      <template v-slot:items="props">
-        <tr :active="props.selected" @click="props.selected = !props.selected">
-          <td>
-            <v-checkbox
-              :input-value="props.selected"
-              primary
-              hide-details
-            ></v-checkbox>
-          </td>
-          <td>{{ props.item.name }}</td>
-          <td class="text-xs-right">{{ props.item.maquina }}</td>
-          <td class="text-xs-right">{{ props.item.dimension }}</td>
-          <td class="text-xs-right">{{ props.item.fallo }}</td>
-          <td class="text-xs-right">{{ props.item.observaciones }}</td>
-        </tr>
-      </template>
-    </v-data-table>
-    <!-- <v-card>
-      <v-list two-line>
-        <template v-for="(pieza, index) in piezas">
-          <v-list-tile :key="pieza.name" avatar>
-            <v-list-tile-content>
-              <v-list-tile-title>Nombre: {{ pieza.name }}</v-list-tile-title>
-              <v-list-tile-sub-title class="text--primary">Máquina: {{ pieza.maquina }}</v-list-tile-sub-title>
-              <v-list-tile-sub-title>Dimension: {{ pieza.dimension }}</v-list-tile-sub-title>
-              <v-list-tile-sub-title v-if="pieza.fallo">Fallo: {{ pieza.fallo }}</v-list-tile-sub-title>
-              <v-list-tile-sub-title v-if="pieza.fallo">Observaciones: {{ pieza.observaciones }}</v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-          <v-divider v-if="index + 1 < piezas.length" :key="index"></v-divider>
-        </template>
-      </v-list>
-    </v-card> -->
+    <v-flex v-if="isMobile">
+      <lista-piezas-mobile/>
+    </v-flex>
+    <v-flex v-else>
+      <lista-piezas-desktop/>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
+import listaPiezasDesktop from '../components/desktop/ListaPiezas.vue'
+import listaPiezasMobile from '../components/mobile/ListaPiezas'
 export default {
   data: () => {
     return {
-      pagination: {
-        sortBy: 'operador'
-      },
-      selected: [],
-      headers: [
-        { text: 'Operador', value: 'operador'
-        },
-        { text: 'Máquina', value: 'maquina' },
-        { text: 'Dimension', value: 'dimension' },
-        { text: 'Fallo', value: 'fallo' },
-        { text: 'Observaciones', value: 'observaciones' }
-      ]
-    }
-  },
-  computed: {
-    piezas () {
-      return this.$store.getters.getListPiezas
-    }
-  },
-  methods: {
-    toggleAll () {
-      if (this.selected.length) this.selected = []
-      else this.selected = this.desserts.slice()
-    },
-    changeSort (column) {
-      if (this.pagination.sortBy === column) {
-        this.pagination.descending = !this.pagination.descending
-      } else {
-        this.pagination.sortBy = column
-        this.pagination.descending = false
-      }
+      isMobile: false
+      // isTablet: false
     }
   },
   created() {
-    this.$store.commit('obtenerPiezas')
+    let mediaQueryMobile = window.matchMedia('(max-width: 700px)')
+    // let mediaQueryTablet = window.matchMedia('(max-width: 1024px)')
+
+    if (mediaQueryMobile.matches)
+      this.isMobile = true
+    // else if (mediaQueryMobile.matches)
+    //   this.isTablet = true
+  },
+  components: {
+    listaPiezasDesktop,
+    listaPiezasMobile
   }
 
 }
